54cd142c11fb1f710660c93c4433b4ea5b573bb6
Amlisha <2302182@dhempecollege.edu.in>
2025-08-19 11:23:19 +0530

    Add files via upload

---
+++ Bytestuff.c
@@ -0,0 +1,31 @@
+//Byte stuffing
+#include <stdio.h>
+#include<string.h>
+
+int main(){
+    char bit[50];
+    char stuff[50];
+    int count=0;
+    int i=0,j=0;
+
+    printf("Input data:");
+    scanf("%s",bit);
+    stuff[j++]='$';
+
+    for(i=0;i<strlen(bit);i++){
+        if(bit[i]=='#' || bit[i]=='$')
+            stuff[j++]='#';
+        stuff[j++]=bit[i];
+    }
+    stuff[j++]='$';
+    stuff[j]='\0';
+
+    printf("bit array:");
+    printf("%s\r\n",bit);
+
+    printf("stuff array:");
+    printf("%s\r\n",stuff);
+    
+    return 0;
+
+}
